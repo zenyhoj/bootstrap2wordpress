59651c48e9f52bd8a7391e49efd3e56c9890f4df
Joe Balingit <joe.balingit@gmail.com>
2017-02-13 14:42:12 +0800

    updated optin section

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    //cache the window object
+    var $window = $(window);
+
+    $('section[data-type="background"]').each(function () {
+
+        var $bgobj = $(this); // assigning the object
+
+        $(window).scroll(function () {
+
+            //scroll the window at var speed
+            //the yPos is negative because we're scrolling it up
+
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            // Put together our final background position
+            var coords = '50% ' + yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+
+        });
+
+    });
+
+});
